3e244b631026ea835d86eeb58087f6d9f18abfa3
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2021-09-13 17:57:58 -0400

    fix amplify export ref

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
 import Amplify from '@aws-amplify/core';
-import awsmobile from './aws-exports';
+import awsmobile from './aws-exports.js';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
