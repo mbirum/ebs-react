78968626117cb4c6931e3d22fb042d984ea45e69
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2021-11-16 23:29:22 -0500

    shipping info widget

--- src/components/store/ShippingAndTerms.js
+++ src/components/store/ShippingAndTerms.js
@@ -5,15 +5,56 @@ import React from 'react';
 
 const ShippingAndTerms = props => {
 
+  function getShippingInfoContent() {
+    var wrapper = document.getElementsByClassName('shipping-info-wrapper')[0];
+    var header = wrapper.firstChild;
+    var content = header.nextSibling;
+    return content;
+  }
+
+  function getExpandableCaret() {
+    return document.getElementsByClassName('expandable-caret')[0];
+  }
+
+  function isShippingInfoActive() {
+    return getShippingInfoContent().classList.contains('shipping-info-content-active');
+  }
+
+  function toggleShippingInfoOn() {
+    getShippingInfoContent().classList.remove('force-hidden-display');
+    setTimeout(() => {
+      getShippingInfoContent().classList.add('shipping-info-content-active');  
+      getExpandableCaret().classList.add('expandable-caret-active');
+    }, 100);
+  }
+
+  function toggleShippingInfoOff() {
+    getShippingInfoContent().classList.remove('shipping-info-content-active');
+  }
+
+  function toggleShippingInfoContent() {
+    if (isShippingInfoActive()) {
+      toggleShippingInfoOff();
+    }
+    else {
+      toggleShippingInfoOn();
+    }
+  }
+
   return (
       <>
       <div className="shipping-info-wrapper">
-          <div className="shipping-info-header">
+          <div className="shipping-info-header" onClick={toggleShippingInfoContent}>
             <span className="shipping-info-header-text">Shipping &amp; Delivery</span>
-            <span className="expandable-caret">&#x2304;</span>
+            <span className="expandable-caret"></span>
           </div>
-          <div className="shipping-info-content">
-              <span>All items will definitely be delivered please trust me i will not doop you. Trust me.</span>
+          <div className="shipping-info-content force-hidden-display">
+              <span>
+                All items will definitely be delivered please trust me i will not doop you. Trust me.
+                All items will definitely be delivered please trust me i will not doop you. Trust me.
+                All items will definitely be delivered please trust me i will not doop you. Trust me.
+                All items will definitely be delivered please trust me i will not doop you. Trust me.
+              </span>
           </div>
       </div>
       </>
